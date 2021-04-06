8ea80e41ff7d061b07f090d250d829931062e32d
ShreyMarwaha <shrey19334@iiitd.ac.in>
2021-04-07 04:06:09 +0530

    implementing line tracing for Aim
    not completed 100%

--- BattleTanks/Source/BattleTanks/Public/TankPlayerController.h
+++ BattleTanks/Source/BattleTanks/Public/TankPlayerController.h
@@ -15,7 +15,23 @@ class BATTLETANKS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
+private:
 	ATank* GetControlledTank() const;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 	
+	//start the tank moving barrel so that a shot would it where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// Return an OUT paramater, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairXLoaction = 0.5;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairYLoaction = 0.5;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 };
